1fe25ef282f6cc6f13946eaa2f0d526eb3634917
Adon Metcalfe <adonm@fastmail.fm>
2024-03-19 11:48:08 +0800

    Added many backends

--- apps/sigmatron.py
+++ apps/sigmatron.py
@@ -3,8 +3,23 @@ import streamlit_permalink as stp
 from streamlit_javascript import st_javascript
 import yaml, micropip
 from pathlib import Path
+
 from sigma.rule import SigmaRule
+
 from sigma.backends.microsoft365defender import Microsoft365DefenderBackend
+
+from sigma.backends.elasticsearch import LuceneBackend
+from sigma.pipelines.elasticsearch.windows import ecs_windows
+from sigma.pipelines.sysmon import sysmon_pipeline
+
+from sigma.backends.splunk import SplunkBackend
+from sigma.pipelines.splunk import splunk_windows_pipeline
+
+from sigma.backends.loki import LogQLBackend
+
+from sigma.backends.carbonblack import CarbonBlackBackend
+from sigma.pipelines.carbonblack import CarbonBlack_pipeline
+from sigma.backends.cortexxdr import CortexXDRBackend
 
 # App configuration
 st.set_page_config(page_title="Sigmatron", layout="wide")
@@ -16,9 +31,9 @@ view_tab, build_tab, extract_tab = st.tabs(["Viewer", "Builder", "Extractor"])
 
 with view_tab:
     # Load sigma rules from the "sigma" directory
-    @st.cache_data
-    def load_rules():
-        rules = {}
+    @st.cache_resource
+    def rule_cache():
+        rules = {}        
         for rule_file in Path("sigma").rglob("*.yml"):
             rule = yaml.safe_load(rule_file.read_text())
             try:
@@ -43,14 +58,28 @@ with view_tab:
             rules[title] = rule_meta
     
         return rules
-    
+
+    @st.cache_resource
+    def backend_cache():
+        # Convenient sigma backends, sourced from popular items at https://github.com/search?q=pysigma-backend&type=repositories&s=stars&o=desc
+        return {
+            "M365 Defender (KQL)": (Microsoft365DefenderBackend(), "kusto"),
+            "Elastic winlogbeat (Lucene)": (LuceneBackend(ecs_windows()), "lucene"),
+            "Elastic sysmon (Lucene)": (LuceneBackend(sysmon_pipeline()), "lucene"),
+            "Splunk": (SplunkBackend(splunk_windows_pipeline()), "splunk"),
+            "Grafana Loki (LogQL)": (LogQLBackend(), "logql"),
+            "Carbon Black (Lucene)": (CarbonBlackBackend(CarbonBlack_pipeline()), "lucene"),
+            "Cortex XDR (XQL)": (CortexXDRBackend(), "xql")
+        }
+
+    # Load rules and backends
+    rules, backends = rule_cache(), backend_cache()
     
     # Option to enter YAML manually
     if stp.toggle("Enter yaml manually", url_key="manual"):
-        sigma_yaml = st.text_area("YAML to convert", url_key="sigma_yaml")
+        sigma_yaml = stp.text_area("YAML to convert", url_key="sigma_yaml")
     else:
-        # Load rules and apply filters
-        rules = load_rules()
+        # Setup default filters
         fltrs, defaults = {}, {"product": ["windows"], "category": ["process_creation"]}
     
         # Sidebar for filters
@@ -78,14 +107,13 @@ with view_tab:
     
     # Sidebar for selecting backend and displaying conversion
     with st.sidebar:
-        backends = {"M365 Defender (KQL)": (Microsoft365DefenderBackend(), "kusto")}
         st.markdown("## Convert and display")
         backend_name = stp.selectbox(f"Sigma Backend ({len(backends)} total)", backends.keys(), url_key="backend")
-        backend, lang = backends[backend_name]
     
     # Convert and display the selected rule
     if "sigma_yaml" in locals() and sigma_yaml is not None:
         try:
+            backend, lang = backends[backend_name]
             converted = backend.convert_rule(SigmaRule.from_yaml(sigma_yaml))[0]
         except Exception as e:
             converted = str(e)
